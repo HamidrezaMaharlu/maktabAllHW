bb0a637706c4d755906ef415ecd25cc922be8012
Hamidreza Maharlou <maharlu.hamidreza@gmail.com>
2022-11-08 19:46:26 +0330

    solve q1-5

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ function Calculator() {
 }
 
 let calc = new Calculator;
-console.log(calc.calculate("3 + 7")); // 10
+// console.log(calc.calculate("3 + 7")); // 10
 
 let powerCalc = new Calculator;
 powerCalc.addMethod("*", (a, b) => a * b);
@@ -97,3 +97,18 @@ let result = powerCalc.calculate("2 ** 3");
 // console.log(result); // 8
 
 //--------------------------------q1-5-------------------------
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(user => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id
+}));
+
+console.log( usersMapped[0].id ); // 1
+console.log( usersMapped[0].fullName ); // John Smith
+
+//----------------------------q1-6-----------------------------
