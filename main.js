f71b2097094f4fff806db7813bc9096c716c7b73
Hamidreza Maharlou <maharlu.hamidreza@gmail.com>
2022-11-15 18:34:00 +0330

    solve q1-3

--- main.js
+++ main.js
@@ -12,10 +12,32 @@ const firstInput = formSearch.querySelector("input")
 const allInputs = formSearch.querySelectorAll("input")
 const lastInput = allInputs[allInputs.length - 1];
 
-//----------------------------q2-----------------------------------------
+//----------------------------q1-2-----------------------------------------
 //select the links
 const externalLink = document.querySelectorAll(`a[href*="://"]:not([href^="http://internal.com"])`)
 //loop over array and change style
-externalLink.forEach(item=>item.style.color="orange")
+externalLink.forEach(item => item.style.color = "orange")
 
-//-----------------------------q3--------------------------------------
+//-----------------------------q1-3--------------------------------------
+function makeList() {
+    //make ul
+    let ul = document.createElement('ul');
+    //select sectionQ3 and append Ul to it
+    document.querySelector(".Q3").append(ul);
+    //while loop to get data from user with prompt
+    while (true) {
+        let data = prompt("Enter the text", "");
+        if (!data) {
+            break;
+        }
+        //make li
+        let li = document.createElement('li');
+        //use textContent to put it as a string
+        li.textContent = data;
+        ul.append(li);
+    }
+}
+
+// makeList();
+
+// ------------------------------q1-4-------------------------------
